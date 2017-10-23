af7bb5c8bcc08ddc957a08917bc664da46fde730
Ollisteka <zhukova.o.m@yandex.ru>
2017-10-23 23:13:35 +0500

    non blocking wait key

--- chip8.py
+++ chip8.py
@@ -273,13 +273,12 @@ class CHIP8:
         :return:
         """
         pressed_key = NOT_A_KEY
-        while pressed_key == NOT_A_KEY:
-            for key in self.keys:
-                if self.keys[key]:
-                    pressed_key = key
-        # if pressed_key == NOT_A_KEY:
-        #     self.registers['pc'] -= 2
-        #     return
+        for key in self.keys:
+            if self.keys[key]:
+                pressed_key = key
+        if pressed_key == NOT_A_KEY:
+            self.registers['pc'] -= 2
+            return
         reg_value = (self.opcode & 0x0F00) >> 8
         self.registers['v'][reg_value] = pressed_key
 
